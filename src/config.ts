4f291fc0efc5cccef526d27d9e14abd0bb661f29
Felipe Barber <barbeariadofelipe2020@gmail.com>
2026-07-02 14:33:22 -0300

    feat: reativa o tema da Copa do Mundo

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // Configurações globais do site
 // Altere as flags para ativar ou desativar os temas sazonais.
 export const IS_SAO_JOAO = false;
-export const IS_COPA = false;
+export const IS_COPA = true;
